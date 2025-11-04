7a2abb4c37c329c3f4d6370b17ba4f2d0482e409
kalambodaniel143 <daniel.kalambo@epitech.eu>
2025-11-04 14:23:14 +0100

    Fix base path for GitHub Pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,10 @@
 export default {
   title: "Documentation de Mon Projet",
+  lang: 'en-US',
+  base: process.env.NODE_ENV === 'production' ? '/Whanos_Documentantion/' : '/',
+  link: '/',
   description: "Une documentation générée avec VitePress",
+  
   themeConfig: {
     nav: [
       { text: "Accueil", link: "/" },
